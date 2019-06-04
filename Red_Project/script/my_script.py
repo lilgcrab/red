78e1d4b8001124327b50d814a2f69d0c7de551ea
lilgcrab <50151408+lilgcrab@users.noreply.github.com>
2019-06-03 17:22:59 -0700

    Update my_script.py

--- Red_Project/script/my_script.py
+++ Red_Project/script/my_script.py
@@ -1,4 +1,11 @@
-from red import start, stop_and_rest, witch_house, go_inside, to_grandmas
+#From template:
+
+import sys
+sys.path.append('../')
+
+
+
+from my_modules.red_functions import start, stop_and_rest, witch_house, go_inside, to_grandmas
 
 #start first function
 start()
