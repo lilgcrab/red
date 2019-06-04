0ab31bf21311b0ccf2f6e169691ddc198f0f639c
lilgcrab <50151408+lilgcrab@users.noreply.github.com>
2019-06-03 17:23:30 -0700

    Update my_script.py

--- Red_Project/script/my_script.py
+++ Red_Project/script/my_script.py
@@ -3,9 +3,7 @@
 import sys
 sys.path.append('../')
 
-
-
-from my_modules.red_functions import start, stop_and_rest, witch_house, go_inside, to_grandmas
+from my_module.red_functions import start, stop_and_rest, witch_house, go_inside, to_grandmas
 
 #start first function
 start()
