1c1a849d030837acec3ae3a8553420fdf99678bc
lilgcrab <50151408+lilgcrab@users.noreply.github.com>
2019-06-04 17:16:17 -0700

    Update my_script.py

--- Red_Project/script/my_script.py
+++ Red_Project/script/my_script.py
@@ -1,9 +1,8 @@
-#From template:
+#From https://stackoverflow.com/questions/7505988/importing-from-a-relative-path-in-python
 
-import sys
-sys.path.append('../')
-
-from my_module.red_functions import start, stop_and_rest, witch_house, go_inside, to_grandmas
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'my_module'))
+from red_functions import start, stop_and_rest, witch_house, go_inside, to_grandmas
 
 #start first function
 start()
